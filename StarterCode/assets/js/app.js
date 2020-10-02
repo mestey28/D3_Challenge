dd24a9506f4da717e055aa03a63136a2ba80c1f4
M Estey <smileyegg@gmail.com>
2020-10-01 21:32:00 -0600

    updated js

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,15 +1,42 @@
 // @TODO: YOUR CODE HERE!
 //  Your Task
-
 // Core Assignment: D3 Dabbler (Required Assignment)
-
 // You need to create a scatter plot between two of the data variables such as `Healthcare vs. Poverty` or `Smokers vs. Age`.
-
 // Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. You'll code this graphic in the `app.js` file of your homework directory—make sure you pull in the data from `data.csv` by using the `d3.csv` function. Your scatter plot should ultimately appear like the image at the top of this section.
-
 // * Include state abbreviations in the circles.
-
 // * Create and situate your axes and labels to the left and bottom of the chart.
-
 // * Note: You'll need to use `python -m http.server` to run the visualization. This will host the page at `localhost:8000` in your web browser.
 
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from data.csv
+d3.csv("..assets/data/data.csv").then(function(data) {
+
+  // Print the Data
+  console.log(data);
